78cf505c053f932fd77f31ea5a3b41c501dffed1
sandro <linder.s@gmx.at>
2022-03-12 23:13:53 +0100

    Implemented socket communication

--- app/src/androidTest/java/com/example/se2einzelphase/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/se2einzelphase/ExampleInstrumentedTest.java
@@ -1,7 +1,5 @@
 package com.example.se2einzelphase;
 
-import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
